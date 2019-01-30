78d4e45b51a010ba3a584557d80d2cb39dc7e20b
voldig <33674048+voldig@users.noreply.github.com>
2019-01-30 18:05:29 +0000

    Tweeter message more readable

--- updates.py
+++ updates.py
@@ -1,7 +1,8 @@
 from sense_hat import SenseHat
-from time import sleep
+import time 
 import tweepy
 from threading import Thread
+
 sense = SenseHat()
 
 e = (0,0,0)
@@ -22,9 +23,52 @@ logo = [
 
 #Twitter 
 def get_api():
-    auth = tweepy.OAuthHandler("password","password")
-    auth.set_access_token("password","password")
+    auth = tweepy.OAuthHandler("password","password") #change that 
+    auth.set_access_token("password-password","password") #change that
     return tweepy.API(auth)
+
+def tweet(tempInt, humInt, presInt):
+    temp = str(tempInt) + "C "
+    humidity = str(humInt) + "% "
+    pressure = str(presInt) + "mbar "
+
+    api = get_api()
+    
+    localtime = time.asctime( time.localtime(time.time()) ) #tweets that are same as previous get blocked - adding time prevents that
+    tweet =  "Local current time :" + localtime + "\n\n" + "The measurements from the room: \n\n Temperature: " + temp + " - " 
+    #advice
+    if tempInt < 18:
+        tweet += "to low temperature."
+    elif tempInt == 21:
+        tweet += "ideal temperature."
+    elif tempInt < 24:
+        tweet += "good room temperature."
+    else:
+        tweet += "to hot stop heating."
+
+    tweet +=  "\n\nPressure: " + pressure +" - " 
+    if presInt < 965:
+        tweet += "it is stormy."
+    elif presInt < 980:
+        tweet += "it is raining."
+    elif presInt < 1005:
+        tweet += "weather is changing."
+    elif presInt < 1030:
+        tweet += "fair."
+    else:
+        tweet += "very dry."
+
+    tweet += "\n\nHumidity is: " + humidity + " - " 
+    if humInt < 30:
+        tweet += "to dry."
+    elif humInt < 50:
+        tweet += "ideal humidity."
+    else: 
+        tweet += "to humid."
+
+    status = api.update_status(status=tweet)
+
+
 
 #Collecting informations 
 def getWeather():
@@ -41,46 +85,12 @@ def getWeather():
     message = temp + humidity + pressure 
 
     sense.show_message(message, scroll_speed=0.1)
-    #Tweet
-    api = get_api()
-
-    tweet = "The current measurements from the room: \n\n The temperature: " + temp + "\n advice: \n" 
-    if tempInt < 18:
-        tweet += "To low temperature - Start heating ASAP!\n"
-    elif tempInt == 21:
-        tweet += "Ideal temperature - Try to keep it this way :)\n"
-    elif tempInt < 24:
-        tweet += "Good room temperature\n"
-    else:
-        tweet += "To hot stop heating\n"
-
-    tweet +=  "\n\nThe pressure: " + pressure +"\n means: \n" 
-    if presInt < 965:
-        tweet += "It is stormy "
-    elif presInt < 980:
-        tweet += "It is raining "
-    elif presInt < 1005:
-        tweet += " Weather is changing"
-    elif presInt < 1030:
-        tweet += "fair"
-    else:
-        tweet += "\n very dry"
-
-    tweet += "\n\nThe humidity is: " + humidity + "\n which is:\n" 
-    if humInt < 30:
-        tweet += "to dry"
-    elif humInt < 50:
-        tweet += "ideal humidity"
-    else: 
-        tweet += "to humid"
-
-    status = api.update_status(status=tweet)
-
+    tweet(tempInt,humInt,presInt)
     sense.set_pixels(logo) #sets the screen of Sense Hat to my logo
     
 if __name__ == '__main__':
     Thread(target = getWeather).start()
-    while True:
-        sleep(1800)#time in seconds until next update
-        Thread(target = getWeather).start()
-
+    # you should use cron job to run this script, but instead you can uncomment following lines:
+#    while True:
+#        time.sleep(1800) #time in seconds until next update
+#        Thread(target = getWeather).start()
